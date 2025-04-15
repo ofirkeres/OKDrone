8c92a983bfcffb939b20616bf18993031eb799da
Ofir Keres <ofiritunes@gmail.com>
2025-04-15 20:07:25 +0300

    Add MPU6050 to Cmake Include Paths & Application Sources

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -22,7 +22,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "mpu6050.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
