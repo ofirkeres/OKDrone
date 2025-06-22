cfe3aff3b8dda48c2acf928dc928c78e16e5016b
Ofir Keres <ofiritunes@gmail.com>
2025-06-22 15:25:55 +0300

    Add Basic (Unfinished) PID Functions (Computation and Initialization).

--- PID/Inc/pid.h
+++ PID/Inc/pid.h
@@ -12,9 +12,38 @@
 /* Includes ------------------------------------------------------------------*/
 
 /* Exported types ------------------------------------------------------------*/
+typedef struct pid_gains
+{
+    float Kp;
+    float Ki;
+    float Kd;
+} pid_gains_t;
+
+typedef struct pid_data
+{
+    float Kp;
+    float Ki;
+    float Kd;
+    float tau;
+    float integrator;
+    float differentiator;
+    float prev_error;
+    float prev_measurement;
+    thresholds_float_t output_thresholds;
+    thresholds_float_t integral_thresholds;
+} pid_data_t;
+
 /* Exported constants --------------------------------------------------------*/
+#define KP (0.0)
+#define KI (0.0)
+#define KD (0.0)
+#define PID_TAU (0.02)
+#define PID_INTEGRAL_MIN_THRESHOLD (-5.0)
+#define PID_INTEGRAL_MAX_THRESHOLD (5.0)
 /* Exported macros -----------------------------------------------------------*/
 /* Exported functions --------------------------------------------------------*/
+void PIDController_Init(pid_gains_t* gains, float tau, thresholds_float_t* output_limits, thresholds_float_t* integral_limits);
+float PIDController_Compute(float set_point, float measurement, float time);
 
 /* Private constants ---------------------------------------------------------*/
 /* Private macros ------------------------------------------------------------*/
--- PID/Src/pid.c
+++ PID/Src/pid.c
@@ -7,6 +7,7 @@
 
 /* Includes ------------------------------------------------------------------*/
 #include "pid.h"
+#include "utilities.h"
 
 /* Exported types ------------------------------------------------------------*/
 /* Exported constants --------------------------------------------------------*/
@@ -16,4 +17,72 @@
 /* Private constants ---------------------------------------------------------*/
 /* Private macros ------------------------------------------------------------*/
 /* Private variables ---------------------------------------------------------*/
+static pid_data_t pid;
 /* Private function ----------------------------------------------------------*/
+
+/**
+ * @brief Initializes the PID Controllers constants.
+ *
+ * @param gains pointer to structure that holds the PID terms values
+ * @param tau
+ * @param output_limits
+ * @param integral_limits
+ */
+void PIDController_Init(pid_gains_t* gains, float tau, thresholds_float_t* output_limits, thresholds_float_t* integral_limits)
+{
+    pid.Kp = gains->Kp;
+    pid.Ki = gains->Ki;
+    pid.Kd = gains->Kd;
+
+    pid.tau = tau;
+
+    pid.integrator = 0.0;
+    pid.differentiator = 0.0;
+    pid.prev_error = 0.0;
+    pid.prev_measurement = 0.0;
+
+    pid.output_thresholds.min = output_limits->min;
+    pid.output_thresholds.max = output_limits->max;
+
+    pid.integral_thresholds.min = integral_limits->min;
+    pid.integral_thresholds.max = integral_limits->max;
+}
+
+/**
+ * @brief
+ *
+ * @param set_point the desired set point/value
+ * @param measurement the measurement from the sensors
+ * @param time measurement time - in seconds
+ * @return computed output signal.
+ */
+float PIDController_Compute(float set_point, float measurement, float time)
+{
+    float error, proportional, output;
+
+    // Error signal
+    error = set_point - measurement;
+
+    // Proportional
+    proportional = pid.Kp * error;
+
+    // Integral
+    pid.integrator += 0.5 * (pid.Ki * time * (error + pid.prev_error));
+    pid.integrator = LIMIT(pid.integrator, pid.integral_thresholds.min, pid.integral_thresholds.max);
+
+    // Derivative (band-limited differentiator)
+    // Note: derivative on measurement, therefore minus sign in front of equation!
+    pid.differentiator = -(2.0 * pid.Kd * (measurement - pid.prev_measurement))
+                          + ((2.0 * pid.tau - time) * pid.differentiator)
+                          / (2.0 * pid.tau + time);
+
+    // Compute output signal and apply limits
+    output = proportional + pid.integrator + pid.differentiator;
+    output = LIMIT(output, pid.output_thresholds.min, pid.output_thresholds.max);
+
+    // Store results for future calculations
+    pid.prev_error = error;
+    pid.prev_measurement = measurement;
+
+    return output;
+}
